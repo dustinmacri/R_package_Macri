4147c9b4e0296695c1e0e3a3cf0a2504be980a83
dustin <dustin.macri@selu.edu>
2021-11-09 20:42:11 +0000

    The data and file for Project Two. Also contains altered .R functions (Expected inputs and outputs)

--- ReadSurveys.R
+++ ReadSurveys.R
@@ -1,2 +1,7 @@
 surveys <- read_csv("/cloud/project/data/surveys.csv") %>% 
   + na.omit()
+
++Expected input: surveys.csv to be read without any NA values
+
++Expected output: function will return with neat data set containing the data of 
+surveys.csv without any NA values
--- SurveysPointPlot.R
+++ SurveysPointPlot.R
@@ -1,1 +1,8 @@
-ggplot(surveys, mapping = aes(x = weight, y = hindfoot_length, color = sex)) + geom_point()+ labs(x = "Weight", y = "Hindfoot Length", color = "Sex") + geom_jitter(alpha = 0.7)
+ggplot(surveys, mapping = aes(x = weight, y = hindfoot_length, color = sex)) + geom_point()+ 
+  labs(x = "Weight", y = "Hindfoot Length", color = "Sex") + geom_jitter(alpha = 0.7)
+
++Expected input: surveys dataframe is used; plots weight as the x value and hindfoot length as 
+the y value.
+
++Expected output: A plot is returned containing data showing the relationship between weight and
+hindfoot length of the surveys dataset.
