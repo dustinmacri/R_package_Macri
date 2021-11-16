8fba988391bf0dfe5d775a2e1186dfbf4b185553
dustin <dustin.macri@selu.edu>
2021-11-16 21:20:25 +0000

    Licensing and such

--- R/ReadSurveys.R
+++ R/ReadSurveys.R
@@ -1,7 +1,6 @@
-surveys <- read_csv("/cloud/project/data/surveys.csv") %>% 
-  + na.omit()
+surveys <- read_csv("/cloud/project/data/surveys.csv") %>% na.omit(surveys)
 
-+Expected input: surveys.csv to be read without any NA values
+#' Expected input: surveys.csv to be read without any NA values
 
-+Expected output: function will return with neat data set containing the data of 
-surveys.csv without any NA values
+#' Expected output: function will return with neat data set containing the data of 
+#' surveys.csv without any NA values
--- R/SurveysLinReg.R
+++ R/SurveysLinReg.R
@@ -4,13 +4,13 @@ ggplot(surveys, mapping = aes(x = weight, y = hindfoot_length, color = sex)) +
   geom_point()+ labs(x = "Weight", y = "Hindfoot Length", color = "Sex") + 
   geom_jitter(alpha = 0.7) + geom_smooth(method = "lm", color = "black")
 
-+Expected inputs: For linear regression, surveys dataset is used, with the regression
-being calculated with the weight and hindfoot length.
-For the plot, surveys dataset is used, weight is used as the x value, hindfoot length
-is used as the y value. A linear regression between weight and hindfoot length is aslo
-inputed.
+#' Expected inputs: For linear regression, surveys dataset is used, with the regression
+#' being calculated with the weight and hindfoot length.
+#' For the plot, surveys dataset is used, weight is used as the x value, hindfoot length
+#' is used as the y value. A linear regression between weight and hindfoot length is aslo
+#' inputed.
 
-+Expected outputs: For linear regression, a linear regression (of course) is produced between
-weight and hindfoot length.
-For the plot, a plot is returned showing the data connecting weight and hindfoot length,
-along with a linear regression between those two characteristics as well.
+#' Expected outputs: For linear regression, a linear regression (of course) is produced between
+#' weight and hindfoot length.
+#' For the plot, a plot is returned showing the data connecting weight and hindfoot length,
+#' along with a linear regression between those two characteristics as well.
